022d6f8c813656e97e580479774cd4fcd8743667
DasJalapa <sistemadasjalapa@gmail.com>
2021-01-19 23:30:48 -0600

    add certified

--- internal/mysql/mysql.go
+++ internal/mysql/mysql.go
@@ -1,10 +1,14 @@
 package mysql
 
 import (
+	"crypto/tls"
+	"crypto/x509"
 	"database/sql"
+	"io/ioutil"
 	"log"
 	"sync"
 
+	"github.com/go-sql-driver/mysql"
 	_ "github.com/go-sql-driver/mysql" //Es el conector para mysql
 
 	"github.com/DasJalapa/reportes-salud/internal/lib"
@@ -24,8 +28,23 @@ func Connect() *sql.DB {
 	server := c.HOSTDB
 	database := c.DATABASE
 
+	rootCert := x509.NewCertPool()
+	pem, err := ioutil.ReadFile("./internal/certificates/mysqlCertified.pem")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if ok := rootCert.AppendCertsFromPEM(pem); !ok {
+		log.Fatal(err)
+	}
+
+	mysql.RegisterTLSConfig("custom", &tls.Config{
+		ServerName: server,
+		RootCAs:    rootCert,
+	})
+
 	once.Do(func() {
-		db, err = sql.Open("mysql", user+":"+password+"@tcp("+server+")/"+database)
+		db, err = sql.Open("mysql", user+":"+password+"@tcp("+server+")/"+database+"?tls=custom")
 		if err != nil {
 			log.Println(err.Error())
 		}
